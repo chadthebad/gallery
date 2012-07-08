f4d94399c55e11edaade3cd7866953a325774a70
Chad Dibble <chad@chadthebad.com>
2012-07-08 14:55:44 -0700

    created scaffolding for photos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = '0e1aca3c9e7103b4b3b2d717ab6392dc54b7779baea8ddd8414ad6962674b4a292fc42f69e84d7f99bdec59cb2e47938cbdc1905f8214bbd6fa0827be0c2dac0'
